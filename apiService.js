09c2d1a391ed218a520fe2f04d96dca4529254c3
frank-wiseki <tgcode208@gmail.com>
2024-07-01 21:08:24 +0100

    fixes

---
+++ apiService.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+class NetworkService {
+    constructor(geoapifyApiKey, weatherApiKey) {
+      this.geoapifyApiKey = geoapifyApiKey;
+      this.weatherApiKey = weatherApiKey;
+    }
+  
+    async fetchUserLocation() {
+      try {
+        const response = await axios.get(`https://api.geoapify.com/v1/ipinfo?&apiKey=${this.geoapifyApiKey}`);
+        return response.data;
+      } catch (error) {
+        console.error('Error fetching location data:', error);
+        throw new Error('Unable to fetch location data');
+      }
+    }
+  
+    async fetchWeather(city) {
+      try {
+        const response = await axios.get(`https://api.weatherapi.com/v1/current.json?q=${city}&key=${this.weatherApiKey}`);
+        return response.data;
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        throw new Error('Unable to fetch weather data');
+      }
+    }
+  }
+
+  module.exports = NetworkService;
---
+++ server.js
@@ -0,0 +1,55 @@
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+const express = require("express");
+const NetworkService = require("./apiService");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const geoApiKey = process.env.GEO_API_KEY;
+const weatherApiKey = process.env.WEATHER_API_KEY;
+
+const networkService = new NetworkService(geoApiKey, weatherApiKey);
+
+app.get("/api/hello", async (req, res) => {
+  try {
+    const visitorName = req.query.visitor_name;
+    const clientIp = req.ip;
+
+    if (!visitorName) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Wrong query, please use 'visitor_name'",
+      });
+    }
+
+    const userLocation = await networkService.fetchUserLocation();
+    const userWeather = await networkService.fetchWeather(
+      userLocation.city.names.en
+    );
+
+    return res.status(200).json({
+      client_ip: clientIp,
+      location: userLocation.city.names.en,
+      message: `Hello ${visitorName}! The temperature is ${userWeather.current.temp_f} in ${userLocation.city.names.en}.`,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: "opps.., you might have hit a wrong endpoint,'try/api/hello'",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
+});
